043c6d00b4f6bfaaa9449f5f42fc4051131f2099
Antonia Verdi <averdi@gm.slc.edu>
2016-05-04 10:56:10 -0500

    finally got images on image page to center properly

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Venue = require('../models/venue');
+var secrets = require('../secrets');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
